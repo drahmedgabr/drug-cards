2f4afdd7424bc7343ab25aaee753464c4e3fe5b0
Ahmed Gabr <ahmadal4ever@gmail.com>
2025-10-27 11:40:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ var printArea = document.getElementById("printArea");
 
 drugs.forEach((drug, index) => {
     
-    drug = JSON.parse(drug);
-
     const card = document.createElement("div");
     card.className = `card ${drug.class}`;
     card.innerHTML = `<h3>${drug.title}</h3><div class="inputDiv"><p>Expire</p><div></div></div><div class="inputDiv"><p>Batch</p><div></div>`;
